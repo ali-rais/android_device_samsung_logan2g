a85697dd3819de99564766b44b72ce3c721a0e41
Aman Kumar <firelord.xda@gmail.com>
2016-04-20 11:05:20 +0530

    logan2g: more permissions and libs to compile
    Fix for wifi, audio lib compile and othershits :)

--- device_logan2g.mk
+++ device_logan2g.mk
@@ -23,6 +23,8 @@ $(call inherit-product-if-exists, vendor/samsung/logan2g/logan2g-vendor.mk)
 # Use the Dalvik VM specific for devices with 512 MB of RAM
 $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 
+$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += device/samsung/logan2g/overlay
 
@@ -48,7 +50,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/ueventd.sc6820i.rc:root/ueventd.sc6820i.rc \
     $(LOCAL_PATH)/rootdir/bin/charge:root/bin/charge \
     $(LOCAL_PATH)/rootdir/bin/poweroff_alarm:root/bin/poweroff_alarm \
-    $(LOCAL_PATH)/rootdir/bin/rawdatad:root/bin/rawdatad 
+    $(LOCAL_PATH)/rootdir/bin/rawdatad:root/bin/rawdatad \
+    $(LOCAL_PATH)/rootdir/init.rc:root/init.rc
 
 # Memtrack
 PRODUCT_PACKAGES += \
@@ -79,47 +82,54 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_para:system/etc/audio_para \
     $(LOCAL_PATH)/configs/codec_pga.xml:system/etc/codec_pga.xml\
-    $(LOCAL_PATH)/configs/tiny_hw.xml:system/etc/tiny_hw.xml
+    $(LOCAL_PATH)/configs/tiny_hw.xml:system/etc/tiny_hw.xml \
+    $(LOCAL_PATH)/configs/audio_hw.xml:system/etc/audio_hw.xml
 
-# These are the hardware-specific files
+# These are the hardware-specific features
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
-    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
-    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
-# Packages
 # Filesystem
 PRODUCT_PACKAGES += \
     setup_fs \
 
-# Camera
+# Wi-Fi
 PRODUCT_PACKAGES += \
-    Gallery2
-
-# Wifi
-PRODUCT_PACKAGES += \
-    wpa_supplicant \
+    macloader \
     dhcpcd.conf \
     hostapd \
+    libnetcmdiface \
+    wpa_supplicant \
     wpa_supplicant.conf
 
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.supplicant_scan_interval=180
 
+# IPv6 tethering
+PRODUCT_PACKAGES += \
+    ebtables \
+    ethertypes
+
 # Audio
 PRODUCT_PACKAGES += \
-    audio.a2dp.default \
-    audio.usb.default
+	audio.a2dp.default \
+	audio.primary.sc6820i \
+	audio_policy.sc6820i \
+	audio.r_submix.default \
+	audio.usb.default \
+	audio_vbc_eq \
+	libaudio-resampler \
+	libatchannel \
+	libtinyalsa
 
 # Usb accessory
 PRODUCT_PACKAGES += \
@@ -166,6 +176,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     mobiledata.interfaces=rmnet0 \
     ro.zygote.disable_gl_preload=true \
     persist.radio.multisim.config=dsds \
+    ro.telephony.ril_class=SamsungSPRDRIL \
+    ro.multisim.simslotcount=2 \
     ro.telephony.call_ring.multiple=0 \
     ro.telephony.call_ring=0 \
     ro.crypto.state=unsupported \
